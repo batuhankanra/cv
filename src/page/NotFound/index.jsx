00dfb3b43a9469eeb3dc904106da7cf04f280d4b
batuhan <batuhan_kanra_60@hotmail.com>
2025-01-30 23:44:04 +0300

    complated notFound

--- src/page/NotFound/index.jsx
+++ src/page/NotFound/index.jsx
@@ -5,10 +5,10 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center min-h-screen  text-center">
       <h1 className="text-9xl font-bold text-vite">404</h1>
       <h2 className="text-3xl font-semibold text-gray-800 dark:text-gray-200 mt-4">
-        Sayfa Bulunamadı!
+        Page Not Found!
       </h2>
       <p className="text-gray-600 dark:text-gray-400 mt-2">
-        Üzgünüz, aradığınız sayfa mevcut değil veya taşınmış olabilir.
+      Sorry, the page you are looking for does not exist or may have moved.
       </p>
     </div>
   )
