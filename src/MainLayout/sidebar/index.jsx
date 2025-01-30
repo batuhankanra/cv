b24f8385dcf55c2e54963c90dfb39bac2690ba7b
batuhan <batuhan_kanra_60@hotmail.com>
2025-01-30 14:41:27 +0300

    responsive updated

--- src/MainLayout/sidebar/index.jsx
+++ src/MainLayout/sidebar/index.jsx
@@ -20,27 +20,29 @@ export default function SideBar() {
 
   }
   return (
-    <div className="sticky top-0 z-10  backdrop-blur-3xl bg-[#E8EAF6]/80 dark:bg-zinc-900/80">
-      <div className=" flex items-center justify-between md:justify-center md:gap-x-10  py-8 text-zinc-500 font-mono tracking-widest" >
+    <div className="sticky  top-0 z-10  backdrop-blur-3xl bg-[#E8EAF6]/80 dark:bg-zinc-900/80">
+      <div className="  w-full flex items-center justify-between md:justify-center md:gap-x-10  py-8 text-zinc-500 font-mono tracking-widest" >
         
-        <div className="relative dark:text-zinc-100 px-5 md:p-0 flex md:hidden items-center flex-col gap-x-2">
+        <div className=" dark:text-zinc-100 px-5 md:p-0 flex md:hidden items-center flex-col gap-x-2">
            <button onClick={()=>setActive(!active)} className="flex "> 
-            <CiMenuBurger />
+            <CiMenuBurger className="mt-1" />
             Batuhan Kanra 
            </button>
-          {active && (
-            <div className="absolute w-full flex flex-col items-center bg-zinc-900 top-5">
-             {categories.map(item=>(
-            <Link 
-            to={item.link} 
-            key={item.id} 
-            className=
-              {`${params.pathname===item.link && 'text-zinc-800 px-2 dark:text-zinc-300'} w-full transition-all duration-200 text-xl font-semibold hover:text-zinc-800 dark:hover:text-zinc-300`}>
-              {item.name}
-            </Link>
-          ))}
+         
+            <div className={`absolute p-2  ${active ? '-left-3' : '-left-40'} transition-all duration-200 p-3    text-left   top-[75px]`}>
+             <div className=" min-h-screen px-4 gap-y-3 flex flex-col  dark:bg-zinc-900 ">
+              {categories.map(item=>(
+                <Link 
+                to={item.link} 
+                key={item.id} 
+                className=
+                  {`${params.pathname===item.link && 'text-zinc-800 px-2 dark:text-zinc-300'} w-full transition-all duration-200 text-xl font-semibold hover:text-zinc-800 dark:hover:text-zinc-300`}>
+                  {item.name}
+                </Link>
+              ))}
+             </div>
             </div>
-          )}
+        
         </div>
         
         <div className="hidden md:flex items-center justify-center gap-x-10">
